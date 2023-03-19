4aafc3e32b9ab3cb7d5f87c34682a64addd9d940
Mader13 <59108331+Mader13@users.noreply.github.com>
2023-03-19 14:42:05 +0600

    cors

--- server.js
+++ server.js
@@ -1,15 +1,9 @@
 const express = require("express");
 const cors = require("cors");
-
 const path = require("path");
 const app = express();
 app.use(express.static(__dirname + "/dist/s3filemanager"));
-app.use(
-  cors({
-    credentials: true,
-    origin: ["https://darling-fudge-0bb332.netlify.app/"],
-  })
-);
+
 app.get("/*", function (req, res) {
   res.sendFile(path.join(__dirname + "/dist/s3filemanager/index.html"));
 });
